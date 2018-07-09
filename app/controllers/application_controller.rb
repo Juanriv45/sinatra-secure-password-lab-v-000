b36e6c8e70a2e0d1621435744d259fa4c4c290da
Juan Rivera <juanriv45@gmail.com>
2018-07-09 03:35:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
     if params[:username] == "" || params[:password] == "" then
       redirect "/failure"
     else
-      user = User.new(:username => params[:username],:password => params[:password])
+      
       redirect "/account"
     end
   end
